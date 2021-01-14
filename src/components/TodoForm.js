e2a7df7e1aac9bea5985b7535d8d3d5bf6669d99
Daniel <daniel-ontiveros@lambdastudents.com>
2021-01-13 19:22:42 -0700

    debugging errors

---
+++ src/components/TodoForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Form = (props) => {
+    return(
+    <form>
+        <input
+        id='newTask'
+        onChange={props.onInputChange}
+        name='task'
+        type='text'
+        placeholder='Add a task to do'
+        />
+        <p>
+            <button id='submit' onClick={props.onSubmit}> Add a task </button>
+        </p>
+        <p>
+            <button id='clear' onClick={props.onClear}> Clear Completed</button>
+        </p>
+    </form>
+    );
+};
+
+
+export default Form;
